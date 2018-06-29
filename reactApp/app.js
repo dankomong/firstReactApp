aef30d69d4849a1b4902e5aa5e57ded198b76e89
danko <dankomong@gmail.com>
2018-06-29 14:45:34 -0700

    first commit

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>WUS GUCCI</p>,
+const dummyData = ['Skydive', 'Play beach volleyball', 'Code some JavaScript']
+
+class Todo extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+      <li><button type="submit" name="X">X</button>{this.props.task}</li>
+    </div>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <ul>
+        {dummyData.map((todo, i) => <Todo task = {todo} key = {i}/>)}
+      </ul>
+    )
+  }
+}
+
+class InputLine extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <p> hi </p>
+    )
+  }
+}
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <p> manny </p>
+    )
+  }
+}
+
+ReactDOM.render(
+  <TodoList />,
    document.getElementById('root'));
